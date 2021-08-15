2b0f46cc432b010f22b34779abff79766278e921
chitwang12 <48553225+chitwang12@users.noreply.github.com>
2021-08-15 16:12:21 +0530

    Delete Chitwan/Arrays&Strings directory

--- Chitwan/Arrays&Strings/Sum.java
+++
@@ -1,7 +0,0 @@
-import util.*;
-public static void main(String[]args){
-        Scanner sc = new Scanner(system.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        System.out.println(a+b);
-        }
