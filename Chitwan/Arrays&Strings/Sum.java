3983e5dbeb0e6e6fea02df596e9e6fc48dc0fd76
chitwang12 <48553225+chitwang12@users.noreply.github.com>
2021-08-15 16:08:33 +0530

    Add files via upload

---
+++ Chitwan/Arrays&Strings/Sum.java
@@ -0,0 +1,7 @@
+import util.*;
+public static void main(String[]args){
+        Scanner sc = new Scanner(system.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a+b);
+        }
