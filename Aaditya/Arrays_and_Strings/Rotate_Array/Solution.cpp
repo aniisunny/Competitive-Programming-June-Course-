7626b4cb24d8453b7f3c48b98b791bd73b7248e3
Aaditya_singh <68394997+Aaditya-Singh78@users.noreply.github.com>
2021-10-25 09:42:11 +0530

    Update Solution.cpp

--- Aaditya/Arrays_and_Strings/Rotate_Array/Solution.cpp
+++ Aaditya/Arrays_and_Strings/Rotate_Array/Solution.cpp
@@ -1,10 +1,16 @@
 class Solution {
 public:
-    void rotate(vector<int>& nums, int k) {
-        k = k%nums.size();
-        while(k--){
-            nums.insert(nums.begin(),nums[nums.size()-1]);
-            nums.pop_back();
+    void rotate(vector<vector<int>>& matrix) {
+      int n = matrix.size();
+        for(int i =0; i<n;i++){
+            for(int j =0;j<i;j++){
+                swap(matrix[i][j],matrix[j][i]);
+                }
+        }
+        for(int i =0; i<n;i++){
+            for(int j =0;j<n/2;j++){
+               swap(matrix[i][j],matrix[i][n-1-j]);
+                }
         }
     }
 };
