a4ebbc7460b9c081bed96cc9296858e4725b0245
Onkar Birajdar <67547781+Onkar-Birajdar@users.noreply.github.com>
2021-11-08 18:34:08 +0530

    started linked list

--- Onkar Birajdar/Linked List/039_Reverse_Linked_List.cpp
+++ Onkar Birajdar/Linked List/039_Reverse_Linked_List.cpp
@@ -4,10 +4,22 @@ Date : 01/11/2021
 Description : Problem Description
 Prob. Link: https://leetcode.com/problems/
 */
-#include<bits/stdc++.h>
+// #include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
+int dif(int arr[],int n){
+    return (*max_element(arr, arr + n) -(*min_element(arr, arr + n)));
+}
 int main() {
-    cokar biradfasf;
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int diff = dif(arr, n);
+    cout << diff;
     return 0;
 }
