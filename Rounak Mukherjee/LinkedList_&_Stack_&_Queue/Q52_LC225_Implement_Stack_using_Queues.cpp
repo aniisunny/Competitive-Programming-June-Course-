6698f56804d0e5ebd6b2ddd9d0bc1c672a9da9d8
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2021-11-22 11:36:22 +0530

    Create Q52_LC225_Implement_Stack_using_Queues.cpp

---
+++ Rounak Mukherjee/LinkedList_&_Stack_&_Queue/Q52_LC225_Implement_Stack_using_Queues.cpp
@@ -0,0 +1,30 @@
+class MyStack {
+    queue<int> q; //using a single queue//
+public:
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        q.push(x);
+        for(int i=0; i<q.size()-1; i++){
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    
+    int pop() {
+        int temp=q.front();
+        q.pop();
+        return temp;
+    }
+    
+    int top() {
+        return q.front();
+    }
+    
+    bool empty() {
+        return q.empty();
+    }
+};
+
