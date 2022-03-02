9619521e45f73d1006ab7c22cc5b780da4dab1de
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2022-03-02 15:28:08 +0000

    Update LC1472.Browser_History.cpp

--- Rounak Mukherjee/Design/LC1472.Browser_History.cpp
+++ Rounak Mukherjee/Design/LC1472.Browser_History.cpp
@@ -1,1 +1,34 @@
 
+class BrowserHistory {
+public:
+    list<string> l;
+    list<string>::iterator it;
+    BrowserHistory(string homepage) {
+        l.push_back(homepage);
+        it=l.begin();
+    }
+    
+    void visit(string url) {
+       list<string>::iterator del=it;
+       del++;
+       l.erase(del,l.end());
+       l.push_back(url);
+       it++; 
+    }
+    
+    string back(int step) {
+        while(it!=l.begin()&&step--)
+        {it--;}
+        return *it;
+    }
+    
+    string forward(int step) {
+          while(it!=(--l.end())&&step-- )
+        {it++;}
+        return *it;
+    }
+};
+
+/**
+using iterator
+ */
