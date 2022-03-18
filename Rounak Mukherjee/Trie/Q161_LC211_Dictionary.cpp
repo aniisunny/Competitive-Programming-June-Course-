0d673512c2be0a499a56fb9f0690765e4bc08b0f
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2022-03-18 03:52:20 +0000

    Update Q161_LC211_Dictionary.cpp

--- Rounak Mukherjee/Trie/Q161_LC211_Dictionary.cpp
+++ Rounak Mukherjee/Trie/Q161_LC211_Dictionary.cpp
@@ -1,68 +1,65 @@
-struct Node{
-  Node *link[26];
-  bool fl=false;
+struct Node {
+    Node* link[26]={NULL};
+    bool flag=false;
     
-  bool contain(char a){
-      return (link[a-'a']!= NULL);
-  }  
-  void put(char a,Node *node) {
-      link[a-'a']=node;
-  } 
-  Node *get(char a){
-      return link[a-'a'];
-  }  
+    bool contain(char a){
+        return (link[a-'a']!=NULL);
+    }
+    void put(char a, Node *node){
+        link[a-'a']=node;
+    }
+     Node* get(char c){
+        return link[c-'a'];
+    }
 };
-
 class WordDictionary {
 public:
-    Node* root;
-    WordDictionary() {
-        root=new Node();
+    Node* root = new Node();
+
+    void addWord( string word) {
+        Node* curV = root;
+        for(int i = 0; i < word.length(); i++) {
+            
+
+			if(!curV->contain(word[i])) {
+                curV->put(word[i], new Node());
+            }
+            
+            curV = curV->get(word[i]);
+        }
+        curV->flag = true;
     }
     
-    void addWord(string word) {
-        int siz=word.length();
-        Node* node=root;
-        for(int i=0;i<siz;i++){
-            if(!node->contain(word[i])){
-                node->put(word[i],new Node());
-            }
-            node=node->get(word[i]);
-        }
-        node->fl=true;
-    }
-    
-     bool helper(string word, Node *node){
-        for(int i=0;i<word.length();i++){
-            if(word[i]=='.'){
-				/*
-					This piece of code is the key to solve this problem 
-					so what this line basically do is it checks if our string has any possibilty 
-					for any character between a to z where we get our desired results. 
-					So we just make a recursive call to the same function for 
-					every possible character at the place of "."!
-				*/
-                for(char ch='a';ch<='z';ch++){
-                    if(node->contain(ch) && helper(word.substr(i+1),node->get(ch))) 
-                        return true;
-                }
-                return false;
-            }
-            else{
-            if(!node->contain(word[i])){
-                return false;
-            }
-            node = node->get(word[i]);
-            }
-        }
-        if(node->fl==true) {
-            return true; 
-        }
-        return false;     
-    }
     bool search(string word) {
-        Node *node  = root;
-        return helper(word,node);
+        return searchHelper(word, 0, root);
+    }
+    
+    bool searchHelper(string word, int index, Node* curV) {
+        // BASE CASE
+        if (index == word.length()) {
+            return curV->flag;
+        }
+
+        
+        char curChar = word[index];
+
+        // when char is defiend
+        if (curChar != '.') {
+            if (curV->contain(curChar))
+                return searchHelper(word, index + 1, curV->get(curChar));
+            return false;
+        }
+        
+        // when char is undefiend
+        for (int i = 0; i < 26; i++) {
+            if (curV->link[i]) {
+                if (searchHelper(word, index + 1, curV->link[i]))
+                    return true;
+            }
+        }
+        return false;
     }
 };
+
+
 
