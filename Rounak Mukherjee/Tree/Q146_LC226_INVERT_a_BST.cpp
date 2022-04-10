f12d063b99ac4177e2c7cfd8b5105773ab8247db
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2022-04-10 09:59:16 +0100

    Update Q146_LC226_INVERT_a_BST.cpp

--- Rounak Mukherjee/Tree/Q146_LC226_INVERT_a_BST.cpp
+++ Rounak Mukherjee/Tree/Q146_LC226_INVERT_a_BST.cpp
@@ -1,1 +1,19 @@
 
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        //base case as if node is NULL then return NULL
+		if(!root)
+            return NULL;
+		
+		//swapping left and right child
+        TreeNode* temp = root->left;
+        root->left = root->right;
+        root->right = temp;
+        //calling recursion on left child
+		invertTree(root->left);
+		//calling recursion on right child
+        invertTree(root->right);
+        return root;
+    }
+};
