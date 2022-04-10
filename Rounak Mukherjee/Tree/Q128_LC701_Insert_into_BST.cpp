da7f5002efd4afc95f7d0328a8bf5b81fff14550
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2022-04-10 08:50:14 +0100

    Update Q128_LC701_Insert_into_BST.cpp

--- Rounak Mukherjee/Tree/Q128_LC701_Insert_into_BST.cpp
+++ Rounak Mukherjee/Tree/Q128_LC701_Insert_into_BST.cpp
@@ -1,1 +1,24 @@
 
+class Solution {
+public:
+  TreeNode* insertIntoBST(TreeNode *root, int val) {
+	if(!root){return new TreeNode(val);}
+      
+    TreeNode* cur=root;
+    while(true){
+        if(cur->val<val){
+            if(!cur->right){cur->right=new TreeNode(val); break;}
+            else{
+              cur=cur->right;  
+            }
+        }
+        else{
+            if(!cur->left){cur->left=new TreeNode(val); break;}
+            else{
+                cur=cur->left;
+            }
+        }
+    } 
+      return root;
+	}
+};
