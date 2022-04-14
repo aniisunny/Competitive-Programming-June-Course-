106dbb3d4a0d27aa89f262ef7d445b74b2f2e75e
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2022-04-14 19:33:38 +0100

    Update Q141_LC116_Populating_Next_Right_Pointers_in_Each_Node.cpp

--- Rounak Mukherjee/Tree/Q141_LC116_Populating_Next_Right_Pointers_in_Each_Node.cpp
+++ Rounak Mukherjee/Tree/Q141_LC116_Populating_Next_Right_Pointers_in_Each_Node.cpp
@@ -1,20 +1,46 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+
    class Solution {
 public:
     Node* connect(Node* root) {
-        if(!root) return nullptr;
+      if(root == NULL) return NULL;
         queue<Node*> q;
-        q.push(root);        
-        while(size(q)) {
-            Node* rightNode = nullptr;                    // set rightNode to null initially
-            for(int i = size(q); i; i--) {                // traversing each level
-                auto cur = q.front(); q.pop();            // pop a node from current level and,
-                cur -> next = rightNode;                  // set its next pointer to rightNode
-                rightNode = cur;                          // update rightNode as cur for next iteration
-                if(cur -> right)                          // if a child exists
-                    q.push(cur -> right),                 // IMP: push right first to do right-to-left BFS
-                    q.push(cur -> left);                  // then push left
+        q.push(root);
+        while(!q.empty()){
+            int size = q.size(); // get size of queue 
+            for(int i=0 ; i < size ; i++){
+                Node* item = q.front(); 
+                if(size - 1 == i) // checking the last value of the level
+                     item -> next = NULL; 
+                
+                q.pop();
+                
+                if(size - 1 != i) // if this is not the last value then previous value will point to next one
+                     item -> next = q.front(); 
+                
+                if(item -> left != NULL)
+                    q.push(item -> left);
+                if(item -> right != NULL)
+                    q.push(item -> right);
             }
-        }
+        } 
         return root;
     }
 };
