5121d0dcf3f2c5b7498d991f683401d8d3aa5bee
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2022-04-15 13:05:32 +0100

    Update Q147_LC669_Trim_BST.cpp

--- Rounak Mukherjee/Tree/Q147_LC669_Trim_BST.cpp
+++ Rounak Mukherjee/Tree/Q147_LC669_Trim_BST.cpp
@@ -1,15 +1,18 @@
 
-  class Solution {
+   class Solution {
 public:
     TreeNode* trimBST(TreeNode* root, int L, int R) {
         if (!root) return root;
         if (root->val >= L && root->val <= R) {
             root->left = trimBST(root->left, L, R);
             root->right = trimBST(root->right, L, R);
-            return root;
+            
         }
-        if (root->val < L)
-            return trimBST(root->right, L, R);
-        return trimBST(root->left, L, R);
+        else if (root->val < L)
+            root= trimBST(root->right, L, R);
+        else if (root->val > R)
+            root= trimBST(root->left, L, R);
+        
+        return root;
     }
 };
