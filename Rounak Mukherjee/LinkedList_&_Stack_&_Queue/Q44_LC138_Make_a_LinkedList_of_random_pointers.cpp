4690110b1c9465b0725ea3cfced83427c9067875
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2022-04-18 15:21:09 +0100

    Update Q44_LC138_Make_a_LinkedList_of_random_pointers.cpp

--- Rounak Mukherjee/LinkedList_&_Stack_&_Queue/Q44_LC138_Make_a_LinkedList_of_random_pointers.cpp
+++ Rounak Mukherjee/LinkedList_&_Stack_&_Queue/Q44_LC138_Make_a_LinkedList_of_random_pointers.cpp
@@ -1,48 +1,49 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    Node* next;
-    Node* random;
-    
-    Node(int _val) {
-        val = _val;
-        next = NULL;
-        random = NULL;
-    }
-};
-*/
-
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-       if(head==NULL){return NULL;}
-        Node *next,*temp;
-    for(Node *curr=head;curr!=NULL;){
-        next=curr->next;
-        curr->next=new Node(curr->val);
-        curr->next->next=next;
-        curr=next;
-    }
-    for(Node *curr=head;curr!=NULL;curr=curr->next->next){
-        curr->next->random=(curr->random!=NULL)?(curr->random->next):NULL;
-    }
-    
-     Node* original = head, *copy = head->next; 
-  
-    temp = copy; 
-  
-    while (original && copy) 
-    { 
-        original->next = 
-         original->next? original->next->next : original->next; 
-  
-        copy->next = copy->next?copy->next->next:copy->next; 
-        original = original->next; 
-        copy = copy->next; 
-    } 
-  
-    return temp;   
+        if(head==NULL){return head;}
+        Node *iter = head; 
+          Node *front = head;
+
+          // First round: make copy of each node,
+          // and link them together side-by-side in a single list.
+          while (iter != NULL) {
+            front = iter->next;
+
+            Node *copy = new Node(iter->val);
+            iter->next = copy;
+            copy->next = front;
+
+            iter = front;
+          }
+
+          // Second round: assign random pointers for the copy nodes.
+          iter = head;
+          while (iter != NULL) {
+            if (iter->random != NULL) {
+              iter->next->random = iter->random->next;
+            }
+            iter = iter->next->next;
+          }
+
+          // Third round: restore the original list, and extract the copy list.
+          iter = head;
+          Node *pseudoHead = new Node(0);
+          Node *copy = pseudoHead;
+
+          while (iter != NULL) {
+            front = iter->next->next;
+
+            // extract the copy
+            copy->next = iter->next;
+
+            // restore the original list
+            iter->next = front;
+              
+            copy = copy -> next; 
+            iter = front;
+          }
+
+          return pseudoHead->next;
     }
 };
