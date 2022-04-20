2ab506af8f5fbcfe3815ac77f586eb47033397b6
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2022-04-20 22:40:59 +0100

    Update Q40_LC234_Palindrome_LinkedList.cpp

--- Rounak Mukherjee/LinkedList_&_Stack_&_Queue/Q40_LC234_Palindrome_LinkedList.cpp
+++ Rounak Mukherjee/LinkedList_&_Stack_&_Queue/Q40_LC234_Palindrome_LinkedList.cpp
@@ -1,26 +1,15 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
       ListNode* reversed(ListNode* head){
-        ListNode* prev=NULL;
-        ListNode* next=NULL;
-
-        while(head!=NULL){
-            next=head->next;
-            head->next=prev;
-            prev=head;
-            head=next;
-        }
-    return prev;
+       if(head->next==NULL){
+           return head;
+       }
+       ListNode* temp=reversed(head->next);
+        
+         ListNode* nex=head->next;
+          nex->next=head;
+          head->next=NULL;
+    return temp;      
     }
     
     bool isPalindrome(ListNode* head) {
@@ -28,12 +17,13 @@ public:
         ListNode* slow=head;
         ListNode* fast=head;
         
-        while(fast!=NULL && fast->next!=NULL){
+        while(fast->next!=NULL && fast->next->next!=NULL){
             slow=slow->next;
             fast=fast->next->next;
         }
         
-        slow=reversed(slow);
+        slow->next=reversed(slow->next);
+        slow=slow->next;
         fast=head;
         
         while(slow!=NULL){
